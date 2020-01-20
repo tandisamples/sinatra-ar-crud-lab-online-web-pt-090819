5db0db4ec39241f7ee3c4716f628f3403020d72d
Tandi Samples <tandisamples@gmail.com>
2020-01-20 15:30:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,34 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+   get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do
+    Article.create(params)
+    redirect '/articles'
+  end
+
+  get '/articles' do 
+    @article = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+
+
+  delete '/posts/:id/delete' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :delete
+  end
 end
---
+++ app/views/new.rb
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action='/articles' method="POST">
+      <label for "tile">title:</label>
+      <input type="text" id="title" name="title"><br />
+      <label for "content">Content:</label>
+      <input type="text" id="content" name="content"><br />
+      <input type="submit" value="submit" id="submit">
+    </form>
+  </body>
+</html>
